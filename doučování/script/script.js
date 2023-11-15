10498de65e5f3e7ab2c4f3d2e5d527d333f97582
jarnivojna <150954219+jarnivojna@users.noreply.github.com>
2023-11-15 21:18:11 +0100

    Add files via upload

--- doučování/script/script.js
+++ doučování/script/script.js
@@ -1,3 +1,69 @@
-const obrazek_1 = "https://th.bing.com/th/id/R.4872bdacdbc1e19fffd22294ddd9de6f?rik=ke%2fJ4t%2bnr9bIDQ&riu=http%3a%2f%2fhdqwalls.com%2fwallpapers%2fguardians-of-the-galaxy-vol-2-cast-lu.jpg&ehk=AQ0P044WXgaShMi9nJn3QChzPHRMVslHrMPjgte5x6s%3d&risl=&pid=ImgRaw&r=0"
-const obrazek_2 = "https://hips.hearstapps.com/esq.h-cdn.co/assets/17/16/1600x800/landscape-1492524044-guardians.jpg?resize=980:*"
-const obrazek_3 = "https://www.billboard.com/wp-content/uploads/media/guardians-of-the-galaxy-billbaord-650.jpg?resize=300"
+const obrazek_1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMBLC8t6JboE29E8wc9m1hpkVdHaloIBKezJ98Y2jcBg&s"
+const obrazek_2 = "https://images.contentstack.io/v3/assets/blt187521ff0727be24/blt8ed0ddc56697f275/60ee12deaa9be3239e98d9a2/Teemo_0.jpg"
+const obrazek_3 = "https://www.billboard.com/wp-content/uploads/media/guardians-of-the-galaxy-billbaord-650.jpg"
+
+function obr1()
+{
+    let img = document.getElementById("obr");
+    
+    if (img.src==obrazek_1)
+    {
+        alert("obrazek_1 už tam je");
+    }
+    else
+    {
+        img.src=obrazek_1;
+    }
+}
+
+function obr2()
+{
+    let img = document.getElementById("obr");
+    
+    if (img.src!=obrazek_2)
+    {
+        img.src=obrazek_2;
+    }
+    else
+    {
+        alert("obrazek_2 už tam je");
+    }
+}
+
+function obr3()
+{
+    let img = document.getElementById("obr")
+
+    if (img.src==obrazek_3)
+    {
+        alert ("obrazek už tam je")
+    }
+    else
+    {
+        img.src=obrazek_3
+    }
+}
+
+function zmena()
+{
+    let input = document.getElementById("input");
+    
+
+    if (input.value=="1")
+    {
+        obr1();
+    }
+    else if (input.value=="2")
+    {
+        obr2();
+    }
+    else if (input.value=="3")
+    {
+        obr3();
+    }
+
+    else
+    {
+        alert("piš jenom 1, 2, 3")
+    }
+}
